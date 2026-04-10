af5f2e54253eab8042b9c4fd1ba312b5c10bef73
Jose Luis Torres Corbalan <jltorres@boxfish.studio>
2026-04-10 08:53:14 +0200

    lint

--- apps/explorer/src/pages/validators/Validators.tsx
+++ apps/explorer/src/pages/validators/Validators.tsx
@@ -316,7 +316,7 @@ function ValidatorPageResult(): JSX.Element {
                     />
                 ) : (
                     <div className="flex w-full flex-col gap-xl">
-                        <div className="dark:text-iota-neutral-92 pt-md--rs text-display-sm text-iota-neutral-10">
+                        <div className="pt-md--rs text-display-sm text-iota-neutral-10 dark:text-iota-neutral-92">
                             Validators
                         </div>
 
