39e89e540c6c3dd46a5b0b7018c36c663e985ef1
evavirseda <evirseda@boxfish.studio>
2026-04-20 16:59:22 +0200

    fix wrong merge

--- apps/names/src/auctions/lib/types/index.ts
+++ apps/names/src/auctions/lib/types/index.ts
@@ -1,13 +1,5 @@
 // Copyright (c) 2025 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-<<<<<<< HEAD:crates/iota-data-ingestion-core/src/reader/mod.rs
-pub(crate) mod common;
-pub(crate) mod fetch;
-pub mod v2;
-
-pub use common::ReaderOptions;
-=======
 export * from './bcs';
 export * from './metadata';
->>>>>>> develop:apps/names/src/auctions/lib/types/index.ts
