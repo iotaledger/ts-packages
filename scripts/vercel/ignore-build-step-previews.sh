3fae2a6a44bc6f336987ee87c1e6d3e6aa3ef199
JCNoguera <88061365+VmMad@users.noreply.github.com>
2026-04-20 16:17:40 +0200

    Update scripts/vercel/ignore-build-step-previews.sh

--- scripts/vercel/ignore-build-step-previews.sh
+++ scripts/vercel/ignore-build-step-previews.sh
@@ -4,7 +4,7 @@
 if [ "$VERCEL_ENV" == "preview" ]; then
   echo "✅ - Preview environment detected. Checking for changes in apps..."
   # Relative to the root directory of the project. E.g relative to apps/wallet-dashboard
-  bash ../../scripts/tooling/vercel/ignore-build-step-base.sh
+  bash ../../scripts/vercel/ignore-build-step-base.sh
 else
   echo "❌ - Not a preview deployment."
   exit 0
