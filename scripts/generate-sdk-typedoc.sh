9867ac6f74b2e243d08a36f291022269a111d5ce
marc2332 <mespinsanz@gmail.com>
2026-04-21 10:17:38 +0200

    feat(ci): Upload generated ts-sdk typedoc to s3 on 'latest' releases

---
+++ scripts/generate-sdk-typedoc.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clean and recreate the output directory
+rm -rf generated-docs
+mkdir -p generated-docs
+
+packages="bcs dapp-kit graphql-transport isc-sdk kiosk ledgerjs-hw-app-iota names signers typescript wallet-standard"
+
+for package in $packages; do
+    # Generate typedoc for the package from its own directory
+    cd "sdk/${package}" || exit
+    ../../node_modules/.bin/typedoc \
+        --options typedoc.json \
+        --plugin typedoc-plugin-markdown \
+        --readme none \
+        --entryFileName index \
+        --out "../../generated-docs/${package}" \
+        --tsconfig tsconfig.json
+    cd - || exit
+done
