707466e6faae60e36d549bbd22a90a4e71abaa45
Juligs <julicodes29@gmail.com>
2026-06-01 10:50:15 +0200

    chore: test commit

--- apps/core/src/components/inputs/AddressInput.tsx
+++ apps/core/src/components/inputs/AddressInput.tsx
@@ -26,7 +26,7 @@ export function AddressInput({
     resolvedNameFieldId,
     disabled,
     placeholder = '0x...',
-    label = 'Enter recipient address or @name',
+    label = 'Enter recipient address or @name a',
 }: AddressInputProps) {
     const { validateField, setFieldValue } = useFormikContext();
     const [field, meta, helpers] = useField<string>(fieldId);
