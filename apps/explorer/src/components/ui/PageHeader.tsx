61782bceb179bb499e2f6d812c03ab7b5ce815fc
Alex Ruzenhack <alex.ruzenhack@gmail.com>
2026-06-15 19:40:53 +0100

    chore: lint style declaration

--- apps/explorer/src/components/ui/PageHeader.tsx
+++ apps/explorer/src/components/ui/PageHeader.tsx
@@ -88,7 +88,7 @@ export function PageHeader({
                             <>
                                 {type && (
                                     <div className="flex flex-row items-center gap-xxs">
-                                        <span className="dark:text-iota-neutral-92 text-headline-sm text-iota-neutral-10">
+                                        <span className="text-headline-sm text-iota-neutral-10 dark:text-iota-neutral-92">
                                             {type}
                                         </span>
                                         {status && (
@@ -104,7 +104,7 @@ export function PageHeader({
                                     </div>
                                 )}
                                 {title && (
-                                    <div className="dark:text-iota-neutral-60 flex items-center gap-xxs text-iota-neutral-40">
+                                    <div className="flex items-center gap-xxs text-iota-neutral-40 dark:text-iota-neutral-60">
                                         <span
                                             className="break-all text-body-ds-lg"
                                             data-testid="heading-object-id"
@@ -125,7 +125,7 @@ export function PageHeader({
                                 {isLoadingSubtitle ? (
                                     <Placeholder width="w-48" />
                                 ) : subtitle ? (
-                                    <span className="dark:text-iota-neutral-60 truncate text-body-md text-iota-neutral-40">
+                                    <span className="truncate text-body-md text-iota-neutral-40 dark:text-iota-neutral-60">
                                         {subtitle}
                                     </span>
                                 ) : null}
