a620bd28f804a7d8cf028f8c2a9f50e788d8cc47
JCNoguera <88061365+VmMad@users.noreply.github.com>
2026-07-22 11:43:52 +0200

    fix: resolve commit sha with vercel envs

--- dapps/multisig-toolkit/src/main.tsx
+++ dapps/multisig-toolkit/src/main.tsx
@@ -17,6 +17,8 @@ import { RouterProvider } from 'react-router-dom';
 import { queryClient } from './lib/queryClient';
 import { router } from './routes';
 
+console.log(`git rev: ${COMMIT_REV}`);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
