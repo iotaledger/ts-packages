d4753b7ff54b846b74a158fbbf8ae1a6e4b6d2c0
Juligs <sjgomez@boxfish.studio>
2026-09-22 09:55:33 +0200

    fix(explorer): fix oversized coin icon on transaction page

--- apps/core/src/components/coin/CoinIcon.tsx
+++ apps/core/src/components/coin/CoinIcon.tsx
@@ -17,13 +17,18 @@ interface NonIotaCoinProps {
 function NonIotaCoin({ coinType, size = ImageIconSize.Full, rounded }: NonIotaCoinProps) {
     const { data: coinMeta } = useCoinMetadata(coinType);
     return (
-        <div className="flex h-full w-full items-center justify-center rounded-full bg-iota-neutral-96 dark:bg-iota-neutral-12">
+        <div
+            className={cx(
+                size,
+                'flex items-center justify-center rounded-full bg-iota-neutral-96 dark:bg-iota-neutral-12',
+            )}
+        >
             <ImageIcon
                 key={coinMeta?.iconUrl}
                 src={coinMeta?.iconUrl}
                 label={coinMeta?.name || coinType}
                 fallback={coinMeta?.name || coinType}
-                size={coinMeta?.iconUrl ? ImageIconSize.Full : size}
+                size={size}
                 fallbackSize={size}
                 rounded={rounded}
             />
